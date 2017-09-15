d7538c81ececb488b73406eca966260b391329f7
Arne Burmeister <arne.burmeister@goeuro.com>
2017-09-15 10:51:30 +0200

    refactoring to support more data providers

--- src/main/java/diergo/junit5/dataprovider/DataGenerator.java
+++ src/main/java/diergo/junit5/dataprovider/DataGenerator.java
@@ -11,11 +11,11 @@ import org.junit.jupiter.api.extension.ParameterResolutionException;
 import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 
+import static java.util.Arrays.copyOf;
 import static org.junit.platform.commons.util.ReflectionUtils.findMethods;
 import static org.junit.platform.commons.util.ReflectionUtils.invokeMethod;
 
@@ -29,22 +29,21 @@ class DataGenerator {
 
     Data generateData(Method testMethod, Class<?> testClass) {
         UseDataProvider useDataProvider = testMethod.getAnnotation(UseDataProvider.class);
-        List<Object[]> data;
-        DataProvider dataProvider;
+        List<DataSet> dataSets = new ArrayList<>();
         if (useDataProvider == null) {
-            dataProvider = testMethod.getAnnotation(DataProvider.class);
-            data = fetchData(dataProvider.value(), dataProvider, testMethod);
+            DataProvider dataProvider = testMethod.getAnnotation(DataProvider.class);
+            dataSets.add(new DataSet(dataProvider, fetchData(dataProvider.value(), dataProvider, testMethod)));
         } else {
             Class<?> dataProviderType = useDataProvider.location().length == 0 ? testClass : useDataProvider.location()[0];
             Method dataProviderMethod = findMethods(dataProviderType, dataProviderFilter(testMethod.getName(), useDataProvider.value()))
                     .stream().findFirst().orElseThrow(
                             () -> new ParameterResolutionException("Cannot find data provider for " + testMethod));
-            dataProvider = dataProviderMethod.getAnnotation(DataProvider.class);
             boolean passMethod = dataProviderMethod.getParameterCount() == 1 && Executable.class.isAssignableFrom(dataProviderMethod.getParameterTypes()[0]);
             Object rawData = passMethod ? invokeMethod(dataProviderMethod, dataProviderType, testMethod) : invokeMethod(dataProviderMethod, dataProviderType);
-            data = fetchData(rawData, dataProvider, testMethod);
+            DataProvider dataProvider = dataProviderMethod.getAnnotation(DataProvider.class);
+            dataSets.add(new DataSet(dataProvider, fetchData(rawData, dataProvider, testMethod)));
         }
-        return new Data(dataProvider, data);
+        return new Data(dataSets);
     }
 
     private List<Object[]> fetchData(Object data, DataProvider dataProvider, Method testMethod) {
@@ -70,30 +69,64 @@ class DataGenerator {
     }
 
     static class Data {
-        private final DataProvider provider;
-        private final List<Object[]> data;
-        private final AtomicInteger i = new AtomicInteger(-1);
+        private final List<DataSet> dataSets;
+        private final AtomicInteger i;
 
-        Data(DataProvider provider, List<Object[]> data) {
-            this.provider = provider;
-            this.data = data;
+        Data(List<DataSet> dataSets) {
+            this.dataSets = dataSets;
+            i = new AtomicInteger(-1);
         }
 
         int getSize() {
-            return data.size();
+            return dataSets.stream().mapToInt(data -> data.data.size()).sum();
         }
 
         Object[] getData(boolean firstParam) {
-            return data.get(firstParam ? i.incrementAndGet() : i.get());
+            int idx = firstParam ? this.i.incrementAndGet() : this.i.get();
+            DataSet set = findDataSet(idx);
+            return set.data.get(findSubIndex(idx));
         }
 
         String createName(Method testMethod, int idx) {
-            String result = provider.format();
+            DataSet set = findDataSet(idx);
+            String result = set.provider.format();
             for (BasePlaceholder placeHolder : Placeholders.all()) {
-                placeHolder.setContext(testMethod, idx, Arrays.copyOf(data.get(idx), data.get(idx).length));
+                Object[] data = set.data.get(findSubIndex(idx));
+                placeHolder.setContext(testMethod, idx, copyOf(data, data.length));
                 result = placeHolder.process(result);
             }
             return result;
+        }
+
+        private DataSet findDataSet(int idx) {
+            int i = idx;
+            for (DataSet set : dataSets) {
+                if (i < set.data.size()) {
+                    return set;
+                }
+                i -= set.data.size();
+            }
+            throw new IndexOutOfBoundsException("No data at index " + idx);
+        }
+
+        private int findSubIndex(int idx) {
+            for (DataSet set : dataSets) {
+                if (idx < set.data.size()) {
+                    return idx;
+                }
+                idx -= set.data.size();
+            }
+            return -1;
+        }
+    }
+
+    static class DataSet {
+        private final DataProvider provider;
+        private final List<Object[]> data;
+
+        DataSet(DataProvider provider, List<Object[]> data) {
+            this.provider = provider;
+            this.data = data;
         }
     }
 }
